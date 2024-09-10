eaf92bad0508d7477505a6651236068790e5e594
Erika <erika@nearcoast.com>
2024-09-10 10:09:25 +0300

    create chat service and chat room

--- lib/page/chat/chat_list_page.dart
+++ lib/page/chat/chat_list_page.dart
@@ -1,14 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_chat/const.dart';
+import 'package:firebase_chat/page/chat/chat_room_page.dart';
 import 'package:flutter/material.dart';
-
 
 class ChatListPage extends StatelessWidget {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-   ChatListPage({super.key});
+  ChatListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,43 +18,58 @@ class ChatListPage extends StatelessWidget {
         centerTitle: true,
         automaticallyImplyLeading: false,
         actions: [
-          InkWell(
-            onTap: () => signOut(),
-              child:const Icon(Icons.door_back_door_outlined))
+          InkWell(onTap: () => signOut(), child: const Icon(Icons.door_back_door_outlined))
         ],
       ),
       body: _buildChatList(),
     );
   }
 
-
-
   Widget _buildChatList() {
     return StreamBuilder(
       stream: _firestore.collection(user).snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return Center(child: Text(snapshot.error.toString()),);
+          return Center(
+            child: Text(snapshot.error.toString()),
+          );
         } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator(),);
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         } else {
           return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
+              itemCount: snapshot.data!.docs.length,
               itemBuilder: (context, index) {
-            final user = snapshot.data!.docs[index];
-            return ListTile(
-              title: Text(user['email'],),
-              subtitle: Text(user['uid']),
+                final user = snapshot.data!.docs[index];
 
-            );
-          });
+                if (user.id == _firebaseAuth.currentUser!.uid) {
+                  return const SizedBox.shrink();
+                }
+                return ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatRoomPage(
+                          userEmail: user['email'],
+                          userUID: user['uid'],
+                        ),
+                      ),
+                    );
+                  },
+                  title: Text(
+                    user['email'],
+                  ),
+                  subtitle: Text(user['uid']),
+                );
+              });
         }
       },
     );
-
   }
 
- void signOut() {
+  void signOut() {
     _firebaseAuth.signOut();
   }
 }
