11996d4186c4ec3d60626f9c4c80114112909322
ihor120495 <froz12345@gmail.com>
2024-09-11 09:58:10 +0300

    refactor code in lodin_page.dart , register_page.dart, register_or_login_page.dart. add action_button.dart

--- lib/page/register_page.dart
+++ lib/page/register_page.dart
@@ -2,10 +2,11 @@ import 'package:firebase_chat/components/custom_text_field_component.dart';
 import 'package:firebase_chat/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:firebase_chat/components/action_button.dart';
 
 class RegisterPage extends StatefulWidget {
-
   final void Function() onTap;
+
   const RegisterPage({super.key, required this.onTap});
 
   @override
@@ -13,90 +14,116 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-final AuthService _authService = AuthService();
+  final AuthService _authService = AuthService();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-final TextEditingController repeatPasswordController = TextEditingController();
-
+  final TextEditingController repeatPasswordController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black12,
       body: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Lottie.asset('assets/animated/Aniki Hamster.json', height: 250),
-              const SizedBox(height: 25),
-              Text(
-                'Register'.toUpperCase(),
-                style: const TextStyle(
-                  fontSize: 40,
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              CustomTextFieldComponent(
-                controller: emailController,
-                label: 'Email',
-                icon: const Icon(Icons.email_outlined),
-                hintText: 'Edter your email',
-              ),
-              const SizedBox(height: 10),
-              CustomTextFieldComponent(
-                controller: passwordController,
-                label: 'Password',
-                icon: const Icon(Icons.remove_red_eye),
-                hintText: 'Edter your password',
-              ),
-              const SizedBox(height: 10),
-              CustomTextFieldComponent(
-                controller: repeatPasswordController,
-                label: 'Repeat Password',
-                icon: const Icon(Icons.remove_red_eye),
-                hintText: 'Enter your password',
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                  onPressed: () => signUp(),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text(
-                      'Sign Up'.toUpperCase(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 19,
-                        fontFamily: 'Montserrat',
-                      ),
-                    ),
-                  )
-              ), const SizedBox(height: 20),
-              TextButton(
-                onPressed: widget.onTap,
-                child: const Text('Login now'),
-              )
-            ],
-          )),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildLottieAnimation(),
+            const SizedBox(height: 25),
+            _buildTitle(),
+            _buildCustomTextField(
+              controller: emailController,
+              label: 'Email',
+              icon: Icons.email_outlined,
+              hintText: 'Enter your email',
+            ),
+            const SizedBox(height: 10),
+            _buildCustomTextField(
+              controller: passwordController,
+              label: 'Password',
+              icon: Icons.remove_red_eye,
+              hintText: 'Enter your password',
+            ),
+            const SizedBox(height: 10),
+            _buildCustomTextField(
+              controller: repeatPasswordController,
+              label: 'Repeat Password',
+              icon: Icons.remove_red_eye,
+              hintText: 'Repeat your password',
+            ),
+            const SizedBox(height: 40),
+            ActionButton(
+              text: 'Sign Up',
+              onPressed: () => signUp(),
+              isSignInButton: false,
+            ),
+            const SizedBox(height: 20),
+            _buildLoginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLottieAnimation() {
+    return Lottie.asset('assets/animated/Aniki Hamster.json', height: 250);
+  }
+
+  Widget _buildTitle() {
+    return Text(
+      'Register'.toUpperCase(),
+      style: const TextStyle(
+        fontSize: 40,
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w700,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildCustomTextField({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+    required String hintText,
+  }) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(icon),
+        hintText: hintText,
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return TextButton(
+      onPressed: widget.onTap,
+      child: const Text('Login now'),
     );
   }
 
   void signUp() async {
     if (passwordController.text != repeatPasswordController.text) {
-      showError();
+      showError('Passwords do not match');
     } else {
-      try{
-       await _authService.signUpWithEmail(email: emailController.text, password: passwordController.text);
+      try {
+        await _authService.signUpWithEmail(
+          email: emailController.text,
+          password: passwordController.text,
+        );
       } catch (error) {
-
+        showError('Signup failed');
       }
     }
   }
 
-   void showError (){
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Паролі не спвпадають')));
-   }
+  void showError(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
 }
