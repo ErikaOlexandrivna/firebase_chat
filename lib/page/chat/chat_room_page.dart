53c19e679cba3ff9a232943c783e44ff117f3844
ihor120495 <froz12345@gmail.com>
2024-09-19 10:01:58 +0300

    refactor code auth service, chat.list, chat room, user_model

--- lib/page/chat/chat_room_page.dart
+++ lib/page/chat/chat_room_page.dart
@@ -1,12 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_chat/services/chat_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_chat/models/user_model.dart';
+import 'package:intl/intl.dart';
 
 class ChatRoomPage extends StatefulWidget {
   final UserModel user;
 
-  const ChatRoomPage({super.key,  required this.user});
+  const ChatRoomPage({super.key, required this.user});
 
   @override
   State<ChatRoomPage> createState() => _ChatRoomPageState();
@@ -49,29 +51,62 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
           );
         } else {
           return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                bool isCurrentUser = snapshot.data!.docs[index]['senderId'] == _firebaseAuth.currentUser!.uid;
-                return Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-                    children: [
-                      Text(snapshot.data!.docs[index]['senderEmail']),
-                      const SizedBox(
-                        height: 5,
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              bool isCurrentUser = snapshot.data!.docs[index]['senderId'] ==
+                  _firebaseAuth.currentUser!.uid;
+              String message = snapshot.data!.docs[index]['message'];
+              String senderEmail = snapshot.data!.docs[index]['senderEmail'];
+              DateTime timestamp = (snapshot.data!
+                  .docs[index]['timestamp'] as Timestamp).toDate();
+
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10, horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: isCurrentUser
+                      ? CrossAxisAlignment.end
+                      : CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      senderEmail,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: isCurrentUser ? Colors.indigo : Colors.teal,
                       ),
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(color: isCurrentUser ? Colors.indigo : Colors.teal),
-                        child: Text(
-                          snapshot.data!.docs[index]['message'],
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              });
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: isCurrentUser ? Colors.indigo[100] : Colors
+                            .teal[100],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: isCurrentUser ? CrossAxisAlignment
+                            .end : CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            message,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            DateFormat('HH:mm, dd MMM').format(timestamp),
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
         }
       },
     );
@@ -79,30 +114,30 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
 
   Widget _buildMessagingInput() {
     return Container(
-      color: Colors.white30,
+      color: Colors.white,
       padding: const EdgeInsets.all(8),
       child: Row(
         children: [
           Expanded(
             child: TextField(
               controller: messageController,
-          decoration: InputDecoration(
-          hintText: 'Type a message',
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide.none,
-          ),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-
-          ),
-
+              decoration: InputDecoration(
+                hintText: 'Type a message',
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16, vertical: 10),
+              ),
             ),
           ),
-
-          const SizedBox(width:8),
+          const SizedBox(width: 8),
           IconButton(
             onPressed: () => sendMessage(),
-            icon: const Icon(Icons.send,color: Colors.blueAccent,),
+            icon: const Icon(Icons.send, color: Colors.blueAccent),
           ),
         ],
       ),
@@ -113,7 +148,7 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
     if (messageController.text.isEmpty) {
       return;
     } else {
-      _chatService.sendMessage(
+      await _chatService.sendMessage(
         message: messageController.text,
         receiverId: widget.user.uid,
       );
